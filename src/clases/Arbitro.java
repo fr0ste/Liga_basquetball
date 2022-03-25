bab3fd559ff2775a51b79ca212d9922ce6a2b6f3
German Vasquez Santiago <you@example.com>
2022-03-24 19:03:17 -0600

    Clase arbitro

---
+++ src/clases/Arbitro.java
@@ -0,0 +1,15 @@
+package clases;
+
+public class Arbitro extends Persona{
+ private String cedula;
+
+public String getCedula() {
+	return cedula;
+}
+
+public void setCedula(String cedula) {
+	this.cedula = cedula;
+}
+ 
+ 
+}
