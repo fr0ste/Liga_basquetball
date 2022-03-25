91cb4af52852c0130d5b7d12252ae17079b61417
German Vasquez Santiago <you@example.com>
2022-03-24 19:59:30 -0600

    avance leer jugador

--- src/Principal/principal.java
+++ src/Principal/principal.java
@@ -1,9 +1,25 @@
 package Principal;
 
+import java.util.Scanner;
+
+import clases.Jugador;
+
+
 public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Jugador jugadores [][] = new Jugador[2][10]; 
+		
+		jugadores[0][i].setNombre(s.next());
+		// se leen loa jugadores del equipo uno.
+		 /*Scanner s = new Scanner(System.in);
+		 //for( int i=0; i<10; i++){
+		 
+			 
+		 }
+		*/
+		
 
 	}
 
