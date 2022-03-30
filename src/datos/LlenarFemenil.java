9071ee0c5cd69fca879eca87615ca8722539072a
froste <73338az@gmail.com>
2022-03-30 15:21:04 -0600

    avance de la clase LlenarFemenil

--- src/datos/LlenarFemenil.java
+++ src/datos/LlenarFemenil.java
@@ -6,6 +6,40 @@ public class LlenarFemenil{
 	public ArrayList<Femenil>lista=new ArrayList<Femenil>();
 	//Método que devuelve un lista
 	public ArrayList<Femenil> LeerJugador(int Max) {
+		
+		String nombre;
+		String domicilio;
+		String telefono;
+		int edad;
+		String nombreEquipo;
+		int j;
+		
+		
+		
+		for(int i=0;i<Max;i++) {
+			
+			j=i+1;
+			
+			Femenil aux = new Femenil();
+			
+			System.out.println("ingrese el nombre del jugador" + j);
+			System.out.println("ingrese el domicilio del jugador" + j);
+			System.out.println("ingrese el telefono del jugador" + j);
+			System.out.println("ingrese la edad del jugador" + j);
+			System.out.println("ingrese el nombre del equipo del jugador" + j);
+			
+			
+			
+			lista.add(aux);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
 		 return lista;
 	 }
 }
