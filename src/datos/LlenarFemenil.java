1396e763e4dc6b3fb9e922ee97d606106a8a32c4
froste <73338az@gmail.com>
2022-03-30 15:06:32 -0600

    cambio de metodos llenar

---
+++ src/datos/LlenarFemenil.java
@@ -0,0 +1,11 @@
+package datos;
+
+import java.util.ArrayList;
+import clases.*;
+public class LlenarFemenil{
+	public ArrayList<Femenil>lista=new ArrayList<Femenil>();
+	//Método que devuelve un lista
+	public ArrayList<Femenil> LeerJugador(int Max) {
+		 return lista;
+	 }
+}
---
+++ src/datos/LlenarVaronil.java
@@ -0,0 +1,11 @@
+package datos;
+
+import java.util.ArrayList;
+import clases.*;
+public class LlenarVaronil {
+	public ArrayList<Varonil>lista=new ArrayList<Varonil>();
+	//Método que devuelve un lista
+	public ArrayList<Varonil> LeerJugador(int MaxArbitro) {
+		 return lista;
+	 }
+}
