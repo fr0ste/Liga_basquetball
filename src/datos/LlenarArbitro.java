d12ddb6be7217abdfd8d56f423d9b3ee497bf7f1
5Nancy <nancy4020ss@gmail.com>
2022-03-30 13:24:28 -0600

    Creación de los métodos leer arbitro y jugador

---
+++ src/datos/LlenarArbitro.java
@@ -0,0 +1,10 @@
+package datos;
+import java.util.ArrayList;
+import clases.*;
+public class LlenarArbitro {
+	public ArrayList<Arbitro>lista=new ArrayList<Arbitro>();
+	//Método que devuelve un lista
+	public ArrayList<Arbitro> LeerArbitro(int MaxArbitro) {
+		 return lista;
+	 }
+}
---
+++ src/datos/LlenarJugador.java
@@ -0,0 +1,11 @@
+package datos;
+
+import java.util.ArrayList;
+import clases.*;
+public class LlenarJugador {
+	public ArrayList<Jugador>lista=new ArrayList<Jugador>();
+	//Método que devuelve un lista
+	public ArrayList<Jugador> LeerJugador(int MaxArbitro) {
+		 return lista;
+	 }
+}
