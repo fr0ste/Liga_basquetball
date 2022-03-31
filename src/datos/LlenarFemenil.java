0d268a6e396c315bbc4de25f39783054fa57537f
5Nancy <nancy4020ss@gmail.com>
2022-03-30 19:11:45 -0600

    Método llenar Femenil

--- src/datos/LlenarFemenil.java
+++ src/datos/LlenarFemenil.java
@@ -1,9 +1,12 @@
 package datos;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import clases.*;
 public class LlenarFemenil{
 	public ArrayList<Femenil>lista=new ArrayList<Femenil>();
+	Scanner entrada=new Scanner(System.in);
 	//Método que devuelve un lista
 	public ArrayList<Femenil> LeerJugador(int Max) {
 		
@@ -23,12 +26,21 @@ public class LlenarFemenil{
 			Femenil aux = new Femenil();
 			
 			System.out.println("ingrese el nombre del jugador" + j);
+			nombre=entrada.nextLine();
 			System.out.println("ingrese el domicilio del jugador" + j);
+			domicilio=entrada.nextLine();
 			System.out.println("ingrese el telefono del jugador" + j);
+			telefono=entrada.nextLine();
 			System.out.println("ingrese la edad del jugador" + j);
+			edad=entrada.nextInt();
 			System.out.println("ingrese el nombre del equipo del jugador" + j);
+			nombreEquipo=entrada.nextLine();
 			
-			
+			aux.setNombre(nombre);
+			aux.setDireccion(domicilio);
+			aux.setTelefono(telefono);
+			aux.setEdad(edad);
+			aux.setNombreequipo(nombreEquipo);
 			
 			lista.add(aux);
 			
